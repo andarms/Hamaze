50abcfd2d7c650a9d7121d2b9ed67cf6580f15bb
Adrian Manjarres <and7702@gmail.com>
2025-07-28 22:57:57 -0500

    enhance Interaction collider position

--- Hamaze.Arpg/Objects/Player/Interaction.cs
+++ Hamaze.Arpg/Objects/Player/Interaction.cs
@@ -10,14 +10,14 @@ namespace Hamaze.Arpg.Objects.Player;
 public class Interaction : GameObject
 {
   readonly Player player;
-  Vector2 size = new(64);
+  Vector2 size = new(32);
   public Interaction(Player player)
   {
     this.player = player;
     Name = "Interaction";
     Collider = new Collider(
       offset: new Vector2(0, 0),
-      size: new Vector2(64)
+      size: size
     )
     {
       DebugColor = Color.Green
@@ -28,7 +28,11 @@ public class Interaction : GameObject
   public override void Update(float dt)
   {
     base.Update(dt);
-    Position = player.FacingDirection * size;
+
+    // Place the interaction area at half of the player's size in the facing direction
+    var playerCenter = player.Collider.Size * 0.5f;
+    var offset = player.FacingDirection * (player.Collider.Size * 0.5f);
+    Position = playerCenter + offset - size * 0.5f;
 
     if (InputManager.IsActionJustPressed("confirm"))
     {
